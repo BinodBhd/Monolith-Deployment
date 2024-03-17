dd322f564cf7a465179dec453bb9052c5196264c
BinodBhd <89444173+BinodBhd@users.noreply.github.com>
2024-03-17 13:19:39 +1100

    Add files via upload

---
+++ Variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "ap-southeast-2"
+}
+
+variable "aws_access_key" {
+  default = "AKIA5FTY77WX2NP3NX5M"
+}
+
+variable "aws_secret_key" {
+  default = "ehbbd2NOgYsg1ir8Q7lMV/S4DkXdGXeiENWRA5fO"
+}
